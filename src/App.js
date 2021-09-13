96c501ab2f7a370ba6ec2cd7c9ee5cd761845669
Anand Shukla <anands2807@gmail.com>
2021-09-13 21:13:08 +0530

    some error

--- src/App.js
+++ src/App.js
@@ -21,14 +21,8 @@ class App extends Component{
     return (
       <div className="App">
         <header className="App-header">
-        <CardList >
-         {this.state.monsters.map(monster => (
-              <h1 key={monster.id}>{monster.name}</h1>
-            ))}
-         </CardList>
-          
-            
-          
+        <CardList monsters = { this.state.monsters }/>
+         
         </header>
       </div>
     );

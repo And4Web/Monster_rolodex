3464f81d23ec81f0f08c02d929ac830d43bc1bf0
Anand Shukla <anands2807@gmail.com>
2021-09-14 08:45:07 +0530

    error solved

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -3,10 +3,10 @@ import { Card } from '../card/card.component';
 import './card-list.style.css';
 
 
-export const CardList = props => {
+export const CardList = props => (
   <div className = "card-list">
     {props.monsters.map(monster => (
     <Card key={monster.id} monster = {monster}/>
   ))}
   </div>
-};
+);
